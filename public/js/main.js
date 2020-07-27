110b7e65256213b3f491f3a03592fcfc324cf105
jefferson-calmon <62778628+jefferson-calmon@users.noreply.github.com>
2020-07-27 20:47:38 -0300

    make it better

---
+++ public/js/main.js
@@ -0,0 +1,106 @@
+var socket = io('/');
+var info = {
+    numberMessages: 0,
+    conected: 0
+}
+var author = ''
+
+
+getAuthor()
+        
+function getAuthor(){
+    let user = localStorage.getItem('user')
+
+    if(user){
+        author = user
+    }
+    else if(!user){
+        toggleBoxForNewUser('tog')
+    }
+}
+
+function renderMessage(message) {
+    $('.messages').append(`<div class="message"><div class="user-image"><i class="fal fa-user-circle"></i></div><div><h2>${message.author}<span> - ${message.time}</span></h2> <p>${message.message}</p></div></div>`)
+
+    info.numberMessages += 1;
+    moveScroll()
+    renderConnectionsInfo()
+};
+
+function renderConnectionsInfo(){
+    $('#online').html(`<h3><i class="fas fa-circle"></i> ${info.conected} Online</h3>`)
+
+    $('#messages-received').html(`<h3 id="messages-received"><i class="fad fa-inbox-in"></i> ${info.numberMessages} Mensagens</h3>`)
+}
+
+function toggleBoxForNewUser(met){
+    if(met === 'tog'){
+        let input = document.getElementById('enter-user');
+        input.classList.toggle('active');
+        input.focus()
+    }
+    if(met === 'get'){
+        let newUser = document.getElementById('input-user').value
+
+        alert(newUser)
+        localStorage.setItem('user', newUser)
+        author = newUser
+        toggleBoxForNewUser('tog')
+    }
+}
+
+socket.on('receivedMessage', function(message){
+    renderMessage(message)
+});
+
+socket.on('previousMessages', function(messages){
+    for (message of messages){
+        renderMessage(message)
+    };
+
+    renderConnectionsInfo()
+
+});
+
+socket.on('ConnectionsInfo', function(connectionsInfo){
+    info.conected = connectionsInfo.connections._connections;
+    renderConnectionsInfo();
+})
+
+function moveScroll(){
+    var objDiv = document.getElementById("messages");
+    objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+function Submit(event){
+
+
+    event.preventDefault();
+
+    getAuthor()
+
+    var message = $('input[name=message]').val()
+    $('#input-message').val('')
+
+    if(message.length){
+        let now = new Date
+        let time = now.getHours() + ':' + now.getMinutes()
+        if (now.getHours() > 12){
+            time += 'pm';
+        }
+        else{
+            time += 'am';
+        };
+
+        var messageObject = {
+            author: author,
+            message: message,
+            time: time
+        }
+
+        renderMessage(messageObject)
+        moveScroll()
+
+        socket.emit('sendMessage', messageObject);
+    }
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+let messages = []
+let connectionsInfo = {
+    connections: 0
+}
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    connectionsInfo.connections = server.getConnections((err, count) => {
+        return count;
+    });
+    
+    socket.emit('ConnectionsInfo', connectionsInfo)
+    socket.emit('previousMessages', messages)
+
+    socket.on('sendMessage', data => {
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    })
+});
+
+server.listen(4000);
